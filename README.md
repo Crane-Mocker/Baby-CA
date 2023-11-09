6f813b68fa90b857bd7fd8bbebbc5abf893a18c6
Crane-Mocker <anaishuangc0conut@gmail.com>
2023-11-09 00:54:54 -0500

    CA and node 1 demo

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
-# centralized-node
+# Baby-CA
+
+This is a baby CA working in lab/intranet
+
+## Communication
 
 ![](pic/baby-CA.png)
+
+**CA**
+
+Generate private key `CAPri.key` and CA cert `CA.pem`. CA acts as root CA.
+
+```bash
+openssl genrsa -des3 -out CAPri.key 2048
+openssl req -x509 -new -nodes -key CAPri.key -sha256 -days 365 -out CA.pem
+```
+
+Then run `Baby-CA.py`
+
+**Node 1**
+
+Generate private key `node1Pri.key` and generate CSR `node1.csr` (cert request)
+
+```bash
+openssl genrsa -out node1Pri.key 2048
+openssl req -new -key node1Pri.key -out node1.csr
+```
+
+**CA**
+
+Generate cert `node1.crt` with CSR
+
+```bash
+openssl x509 -req -in node1.csr -CA CA.pem -CAkey CAPri.key -CAcreateserial -out node1.crt -days 365 -sha256
+```
+
+**Node 1**
+
+Sign `to-sign.txt` using the private key `node1Pri.key`. Get the signature `signature.txt`
+
+```bash
+openssl dgst -sha256 -sign node1Pri.key -out signature.txt to-sign.txt
+```
+
+**Node 2**
+
+To verify the signature `signature.txt`, first load the cert `node1.crt`, then verify. If `to-sign.txt` is changed, verification failure will occur.
+
+```bash
+openssl x509 -in node1.crt
+openssl dgst -sha256 -verify <(openssl x509 -in node1.crt -pubkey -noout) -signature signature.txt to-sign.txt
+```
+
+## Use
+
+**CA**
+
+```bash
+openssl genrsa -des3 -out CAPri.key 2048
+openssl req -x509 -new -nodes -key CAPri.key -sha256 -days 365 -out CA.pem
+```
---
+++ node1-demo.py
@@ -0,0 +1,65 @@
+import socket
+import os
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Configuration
+baby_ca_address = 'localhost'
+baby_ca_port = 3000
+
+# Generate a private key for Node 1
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+# Write private key to file
+with open('node1Pri.key', 'wb') as f:
+    f.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
+print("Private key for node 1 generated")
+
+# Create a CSR
+print("Creating CSR...")
+csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+    # CSR info
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"US"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"California"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"San Francisco"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"c0conut"),
+    x509.NameAttribute(NameOID.COMMON_NAME, u"node1.c0conut.com"),
+])).add_extension(
+    x509.SubjectAlternativeName([x509.DNSName(u"node1.c0conut.com")]),
+    critical=False,
+    # Sign the CSR with our private key.
+).sign(private_key, hashes.SHA256(), default_backend())
+print("CSR created.")
+
+# Write CSR to file
+with open('node1.csr', 'wb') as f:
+    f.write(csr.public_bytes(serialization.Encoding.PEM))
+
+# Send the CSR to the Baby CA
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((baby_ca_address, baby_ca_port))
+    print("Sending CSR...")
+    sock.sendall(csr.public_bytes(serialization.Encoding.PEM))
+    print("CSR sent!")
+
+    # Receive data from the server and shut down
+    received = sock.recv(2048)
+    print("Certificate received from Baby CA:")
+
+# Write received certificate to file
+with open('node1.crt', 'wb') as f:
+    f.write(received)
+
+print("Certificate saved as node1.crt")
